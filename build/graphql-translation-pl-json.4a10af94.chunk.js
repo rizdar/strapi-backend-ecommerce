6accfbb9c5f822a3a23e4866f9ebdac19db7a857
Rizki Darmawan <rizdar.contact@gmail.com>
2023-02-04 13:15:52 +0700

    litle change

---
+++ build/graphql-translation-pl-json.4a10af94.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbackend=self.webpackChunkbackend||[]).push([[3382],{71097:e=>{e.exports=JSON.parse('{"plugin.description.long":"Dodaje GraphQL endpoint z domy\u015Blnymi metodami API.","plugin.description.short":"Dodaje GraphQL endpoint z domy\u015Blnymi metodami API."}')}}]);
---
+++ build/graphql-translation-sv-json.0c9cc3ab.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbackend=self.webpackChunkbackend||[]).push([[6068],{73287:e=>{e.exports=JSON.parse('{"plugin.description.long":"L\xE4gger till en GraphQL-rutt med standard API-metoder.","plugin.description.short":"L\xE4gger till en GraphQL-rutt med standard API-metoder."}')}}]);
